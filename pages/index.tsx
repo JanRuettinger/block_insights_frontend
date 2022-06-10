d87b35d0a1f529417ba382689a576da1a6531501
Jan Ruettinger <service+github@janruettinger.com>
2022-06-10 14:12:46 -0700

    add fake data for Demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,67 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { allowedNodeEnvironmentFlags } from 'process';
 import { useState } from 'react';
 import ResultSection from '../components/ResultSection';
 
+const defiExperienceChart1 = [
+    { y: 43, label: 'did <5 DeFi transactions' },
+    { y: 18, label: 'did <100 DeFi transactions' },
+    { y: 39, label: 'did >100 DeFi transactions' },
+];
+
+const defiExperienceChart2 = [
+    { y: 7, label: 'did <5 DeFi transactions' },
+    { y: 50, label: 'did <100 DeFi transactions' },
+    { y: 43, label: 'did >100 DeFi transactions' },
+];
+
+const ethBalanceChart1 = [
+    { y: 83, label: 'own 0-1 ETH' },
+    { y: 8, label: 'own 1-10 ETH' },
+    { y: 9, label: 'own >10 ETH' },
+];
+
+const ethBalanceChart2 = [
+    { y: 43, label: 'own 0-1 ETH' },
+    { y: 18, label: 'own 1-10 ETH' },
+    { y: 39, label: 'own >10 ETH' },
+];
+
+const web3experienceChart1 = [
+    { y: 60, label: 'joined <1 ago' },
+    { y: 20, label: 'joined between 1 and 2 years ago' },
+    { y: 20, label: 'joined >2 years ago' },
+];
+
+const web3experienceChart2 = [
+    { y: 23, label: 'joined <1 ago' },
+    { y: 8, label: 'joined <2 years ago' },
+    { y: 79, label: 'joined >2 years ago' },
+];
+
+const otherNFTChart1 = [
+    { y: 43, label: 'own <5 other NFTs' },
+    { y: 18, label: 'own <10 other NFTs' },
+    { y: 39, label: 'own >10 other NFTs' },
+];
+
+const otherNFTChart2 = [
+    { y: 17, label: 'own <5 other NFTs' },
+    { y: 23, label: 'own <10 other NFTs' },
+    { y: 60, label: 'own >10 other NFTs' },
+];
+
 const Home: NextPage = () => {
     const [showResults, setShowResults] = useState<boolean>(false);
-
-    const defiExperienceChart = [
-        { y: 43, label: 'own <5 other NFTs' },
-        { y: 18, label: 'own <10 other NFTs' },
-        { y: 39, label: 'own >10 other NFTs' },
-    ];
-
-    const ethBalanceChart = [
-        { y: 43, label: 'own <5 other NFTs' },
-        { y: 18, label: 'own <10 other NFTs' },
-        { y: 39, label: 'own >10 other NFTs' },
-    ];
-
-    const web3experienceChart = [
-        { y: 43, label: 'own <5 other NFTs' },
-        { y: 18, label: 'own <10 other NFTs' },
-        { y: 39, label: 'own >10 other NFTs' },
-    ];
-
-    const otherNFTChart = [
-        { y: 43, label: 'own <5 other NFTs' },
-        { y: 18, label: 'own <10 other NFTs' },
-        { y: 39, label: 'own >10 other NFTs' },
-    ];
+    const [resultState, setResultState] = useState<number>(0);
+    const [defiExperience, setDefiExperience] =
+        useState<any>(defiExperienceChart1);
+    const [web3Experience, setWeb3Experience] =
+        useState<any>(web3experienceChart1);
+    const [nftExperience, setNftExperience] = useState<any>(otherNFTChart1);
+    const [ethBalance, setEthBalance] = useState<any>(ethBalanceChart1);
 
     const Results = () => {
         if (showResults) {
@@ -44,19 +76,19 @@ const Home: NextPage = () => {
                     </div>
                     <ResultSection
                         title="Do the current token holders have experience in DeFi?"
-                        dataPoints={otherNFTChart}
+                        dataPoints={defiExperience}
                     />
                     <ResultSection
                         title="How much ETH do the current token holders have?"
-                        dataPoints={otherNFTChart}
+                        dataPoints={ethBalance}
                     />
                     <ResultSection
                         title="When did the current token holders start using web3?"
-                        dataPoints={otherNFTChart}
+                        dataPoints={web3Experience}
                     />
                     <ResultSection
                         title="Do the current token holders collect other NFTs?"
-                        dataPoints={otherNFTChart}
+                        dataPoints={nftExperience}
                     />
                 </div>
             );
@@ -66,7 +98,7 @@ const Home: NextPage = () => {
     };
 
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col min-h-screen">
             <Head>
                 <title>Block Insights</title>
                 <meta
@@ -87,6 +119,21 @@ const Home: NextPage = () => {
                         type="text"
                         placeholder="Contract address"
                         className="border-2 border-gray-400 rounded-md p-2"
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            if (e.target.value) {
+                                setDefiExperience(defiExperienceChart1);
+                                setEthBalance(ethBalanceChart1);
+                                setWeb3Experience(web3experienceChart1);
+                                setNftExperience(otherNFTChart1);
+                            } else {
+                                setShowResults(false);
+                                setDefiExperience(defiExperienceChart2);
+                                setEthBalance(ethBalanceChart2);
+                                setWeb3Experience(web3experienceChart2);
+                                setNftExperience(otherNFTChart2);
+                            }
+                        }}
                     />
                     <div className="ml-2 border-2 border-gray-400 p-2 rounded-md">
                         Ethereum dropdown
